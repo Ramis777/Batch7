1eb429aac048852bd4f7cbdb0319b9555abef4b2
ahmedfnu <s700ns@gmail.com>
2021-03-14 13:34:58 -0500

    some changes

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Techtorial");
         System.out.println("mamo");
         System.out.println("look look mamo");
+        System.out.println("whatsup whatsup");
     }
 }
